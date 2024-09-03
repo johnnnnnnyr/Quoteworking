c9c2af900cdf2ebb8824150aeb68d8fbefd9019b
johnnnnnnyr <139003001+johnnnnnnyr@users.noreply.github.com>
2024-09-03 16:36:20 +0500

    responsivness

--- main.py
+++
@@ -1,8 +0,0 @@
-import requests
-category = 'happiness'
-api_url = 'https://api.api-ninjas.com/v1/quotes?category={}'.format(category)
-response = requests.get(api_url, headers={'X-Api-Key': 'VqTJH5iFm+eaVMB2Eibyyw==lXktjEyf3sGpOEBf'})
-if response.status_code == requests.codes.ok:
-    print(response.text)
-else:
-    print("Error:", response.status_code, response.text)
