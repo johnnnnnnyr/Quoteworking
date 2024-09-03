65a4debe3bdca2bfee0d70512e9de4ec3b379ce9
johnnnnnnyr <139003001+johnnnnnnyr@users.noreply.github.com>
2024-09-03 16:24:00 +0500

    changed api
    changes to the api

--- script.js
+++ script.js
@@ -2,17 +2,26 @@ let quote = document.getElementById("quote");
 let author = document.getElementById("author");
 let btn = document.getElementById("btn");
 
-const url = "https://api.quotable.io/random";
+const url = "https://api.api-ninjas.com/v1/quotes";
+const apiKey = "VqTJH5iFm+eaVMB2Eibyyw==lXktjEyf3sGpOEBf"; // replace with your API key
 
 let getQuote = () => {
-  fetch(url)
+  fetch(url, {
+    headers: {
+      'X-Api-Key': apiKey
+    }
+  })
     .then((data) => data.json())
-    .then((item) => {
-      quote.innerText = item.content;
-      author.innerText = item.author;
+    .then((items) => {
+      const randomQuote = items[0]; // select the first quote from the array
+      quote.innerText = randomQuote.quote;
+      author.innerText = randomQuote.author;
+    })
+    .catch((error) => {
+      console.error(error);
+      // handle error here, for example, display an error message to the user
     });
 };
-
 
 window.addEventListener("load", getQuote);
 btn.addEventListener("click", getQuote);
